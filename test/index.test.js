e699550574452e55cc99a3b9e615ee88c121ab65
Wisely Kwek <wisely.kwek.2020@scis.smu.edu.sg>
2022-09-01 11:20:47 +0800

    added function to index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -1,25 +1,39 @@
-const { areaOfARectangle, areaOfATriangle, daysBetweenDates } = require('../webapp/index.js');
+function calculateDaysBetweenDates(begin, end) {
+    var beginDate = new Date(begin);
+    var endDate = new Date(end);
+    var timeDiff = endDate.getTime() - beginDate.getTime();
+    return Math.ceil(timeDiff / (1000 * 3600 * 24));
+}
+
+function calculateAreaOfTriangle(length, height) {
+    return length * height / 2;
+}
+
+function calculateAreaOfRectangle(length, width) {
+    return length * width;
+}
+
 describe("Testing Area of A Triangle", () => {
     it("should return the area of a triangle", () => {
-        expect(areaOfATriangle(3, 4)).toBe(6);
-        expect(areaOfATriangle(10, 10)).toBe(50);
-        expect(areaOfATriangle(2, 2)).toBe(4);
+        expect(calculateAreaOfTriangle(3, 4)).toBe(6);
+        expect(calculateAreaOfTriangle(10, 10)).toBe(50);
+        expect(calculateAreaOfTriangle(2, 2)).toBe(2);
     });
 } );
 
 describe("Testing Area of A Rectangle", () => {
     it("should return the area of a rectangle", () => {
-        expect(areaOfARectangle(3, 4)).toBe(12);
-        expect(areaOfARectangle(10, 10)).toBe(100);
-        expect(areaOfARectangle(2, 2)).toBe(4);
+        expect(calculateAreaOfRectangle(3, 4)).toBe(12);
+        expect(calculateAreaOfRectangle(10, 10)).toBe(100);
+        expect(calculateAreaOfRectangle(2, 2)).toBe(4);
     } );
 } );
 
 describe("Testing Days Between Dates", () => {
     it("should return the number of days between two dates", () => {
-        expect(daysBetweenDates("01/01/2019", "01/01/2019")).toBe(0);
-        expect(daysBetweenDates("01/01/2019", "01/02/2019")).toBe(1);
-        expect(daysBetweenDates("01/01/2019", "01/03/2019")).toBe(2);
-        expect(daysBetweenDates("01/01/2019", "01/04/2019")).toBe(3);
+        expect(calculateDaysBetweenDates("01/01/2019", "01/01/2019")).toBe(0);
+        expect(calculateDaysBetweenDates("01/01/2019", "01/02/2019")).toBe(1);
+        expect(calculateDaysBetweenDates("01/01/2019", "01/03/2019")).toBe(2);
+        expect(calculateDaysBetweenDates("01/01/2019", "01/04/2019")).toBe(3);
     } );
 } );
